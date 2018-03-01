0f7a322f3ab97de1b298790e29c3fae1031e9f2b
mirok <miroslav.kuchta@gmail.com>
2018-03-01 17:15:39 +0100

    update

--- fenics_ii/reduced_eig/test.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import splitext, join
-
-module = '.'
-ignore = ('test.py')
-for f in os.listdir(module):
-    if splitext(f)[1] == '.py' and f not in ignore:
-        print '\t', f
-        execfile(join('.', module, f))
