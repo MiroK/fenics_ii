13f6587f75602bb03ba37e69dc45e5958dc07272
mirok <miroslav.kuchta@gmail.com>
2018-03-12 16:37:41 +0100

    update

---
+++ test/vector_trace.py
@@ -0,0 +1,43 @@
+from dolfin import *
+from xii.assembler.trace_matrix import trace_mat
+from xii import EmbeddedMesh
+
+
+mesh = UnitSquareMesh(10, 10)
+
+bdry = FacetFunction('size_t', mesh, 0)
+DomainBoundary().mark(bdry, 1)
+bmesh = EmbeddedMesh(bdry, 1)
+
+V = FunctionSpace(mesh, 'CG', 2)
+TV = FunctionSpace(bmesh, 'CG', 2)
+
+Trace = trace_mat(V, TV, bmesh, {'restriction': '', 'normal': None})
+
+f = Expression('sin(pi*(x[0]+x[1]))', degree=3)
+
+v = interpolate(f, V)
+Tv0 = interpolate(f, TV)
+
+Tv = Function(TV)
+Trace.mult(v.vector(), Tv.vector())
+
+Tv0.vector().axpy(-1, Tv.vector())
+print Tv0.vector().norm('linf')
+
+
+V = VectorFunctionSpace(mesh, 'CG', 2)
+TV = VectorFunctionSpace(bmesh, 'CG', 2)
+
+Trace = trace_mat(V, TV, bmesh, {'restriction': '', 'normal': None})
+
+f = Expression(('sin(pi*(x[0]+x[1]))', 'x[0]+x[1]'), degree=3)
+
+v = interpolate(f, V)
+Tv0 = interpolate(f, TV)
+
+Tv = Function(TV)
+Trace.mult(v.vector(), Tv.vector())
+
+Tv0.vector().axpy(-1, Tv.vector())
+print Tv0.vector().norm('linf')
---
+++ test/vectorize.py
@@ -0,0 +1,32 @@
+from dolfin import *
+from xii import inverse, VectorizedOperator
+from hsmg import HsNorm
+import numpy as np
+
+
+mesh = UnitSquareMesh(10, 10)
+VV = FunctionSpace(mesh, MixedElement([FiniteElement('Lagrange', triangle, 1)]*3))
+u, v = TrialFunction(VV), TestFunction(VV)
+M = assemble(inner(u, v)*dx)
+
+x = M.create_vec()
+x.set_local(np.random.rand(x.local_size()))
+
+y = M*x
+
+V = FunctionSpace(mesh, 'CG', 1)
+u, v = TrialFunction(V), TestFunction(V)
+Mj = assemble(inner(u, v)*dx)
+# Should match M
+X = VectorizedOperator(Mj, VV)
+y0 = X*x
+
+print x.norm('l2'), (y - y0).norm('linf')
+
+I = HsNorm(V, s=0.5)
+II = VectorizedOperator(I, VV)
+
+foo = II*x
+x0 = inverse(II)*foo
+
+print (x - x0).norm('linf')
