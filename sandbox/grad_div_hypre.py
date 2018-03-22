e242874239726668d26bc73ce5bb227d13ae5594
mirok <miroslav.kuchta@gmail.com>
2018-03-22 13:18:41 +0100

    2d AMS works

--- sandbox/grad_div_hypre.py
+++ sandbox/grad_div_hypre.py
@@ -1,57 +1,117 @@
 from dolfin import *
 from petsc4py import PETSc
+from mpi4py import MPI as pyMPI
+from sympy.printing import ccode
+import sympy as sp
 
-n = 32
+# MMS utils
+def expr_body(expr, **kwargs):
+    if not hasattr(expr, '__len__'):
+        # Defined in terms of some coordinates
+        xyz = set(sp.symbols('x[0], x[1], x[2]'))
+        xyz_used = xyz & expr.free_symbols
+        assert xyz_used <= xyz
+        # Expression params which need default values
+        params = (expr.free_symbols - xyz_used) & set(kwargs.keys())
+        # Body
+        expr = ccode(expr).replace('M_PI', 'pi')
+        # Default to zero
+        kwargs.update(dict((str(p), 0.) for p in params))
+        # Convert
+        return expr
+    # Vectors, Matrices as iterables of expressions
+    else:
+        return [expr_body(e, **kwargs) for e in expr]
 
-mesh = UnitSquareMesh(n, n)
-V = FunctionSpace(mesh, 'RT', 1)
 
-u, v = TrialFunction(V), TestFunction(V)
+def as_expression(expr, degree=4, **kwargs):
+    '''Turns sympy expressions to Dolfin expressions.'''
+    return Expression(expr_body(expr), degree=degree, **kwargs)
 
-f = Expression(('sin(pi*(x[0]+x[1])', 'sin(pi*(x[0]+x[1])')), degree=3)
 
-a = inner(div(u), div(v))*dx + inner(u, v)*dx
-L = inner(f, v)
+def main(n):
+    '''Solves grad-div problem in 2d with HypreAMS preconditioning'''
+    # Exact solution
+    x, y = sp.symbols('x[0] x[1]')
+    
+    sigma = sp.Matrix([sp.cos(pi*y**2), sp.sin(pi*x**2)])
 
-A, b = assemble_system(a, L, bc)
+    sp_div = lambda f: f[0].diff(x, 1) + f[1].diff(y, 1)
 
-# Solve
-# The gradient matrix
-Q = FunctionSpace(mesh, 'CG', 1)
-p = TrialFunction(Q)
+    sp_grad = lambda f: sp.Matrix([f.diff(x, 1), f.diff(y, 1)])
+    
+    f = -sp_grad(sp_div(sigma)) + sigma
 
-G = assemble(inner(v, grad(p))*dx)
+    sigma_expr, f_expr = map(as_expression, (sigma, f))
 
-# CG Solver
-ksp = PETSc.KSP().create(PETSc.COMM_WORLD)
-ksp.setType('cg')
-ksp.setTolerances(rtol=1E-8, atol=1E-12, divtol=1.0E10, max_it=300)
+    # The discrete problem
+    mesh = UnitSquareMesh(n, n)
+    
+    V = FunctionSpace(mesh, 'RT', 1)
+    u, v = TrialFunction(V), TestFunction(V)
 
-# AMS preconditioner
-pc = ksp.getPC()
-pc.setType('hypre')
-pc.setHYPREType('ams')
+    a = inner(div(u), div(v))*dx + inner(u, v)*dx
+    L = inner(f_expr, v)*dx
 
-vec = lambda x: as_backend_type(x).vec()
-mat = lambda A: as_backend_type(A).mat()
+    A, b = assemble_system(a, L)
 
-# Attach gradient
-pc.setHYPREDiscreteGradient(mat(G))
+    # Solve
+    Q = FunctionSpace(mesh, 'CG', 1)
+    G = DiscreteOperators.build_gradient(V, Q)
 
-# Constant nullspace (in case not mass and bcs)
-constants = [vec(interpolate(c, V).vector())
-             for c in (Constant((1, 0)), Constant((0, 1)))]
+    ksp = PETSc.KSP().create(PETSc.COMM_WORLD)
+    ksp.setType('cg')
+    ksp.setTolerances(rtol=1E-8, atol=1E-12, divtol=1.0E10, max_it=300)
 
-pc.setHYPRESetEdgeConstantVectors(*constants)
+    opts = PETSc.Options()
+    opts.setValue("ksp_monitor_true_residual", None)
+    ksp.setFromOptions()
 
-# pc.setHYPRESetBetaPoissonMatrix(None)
+    # AMS preconditioner
+    pc = ksp.getPC()
+    pc.setType('hypre')
+    pc.setHYPREType('ams')
 
-# Set operator for the linear solver
-ksp.setOperators(mat(A))
+    vec = lambda x: as_backend_type(x).vec()
+    mat = lambda A: as_backend_type(A).mat()
 
-uh = Function(V)
+    # Attach gradient
+    pc.setHYPREDiscreteGradient(mat(G))
 
-ksp.solve(vec(b), vec(uh))
+    # Constant nullspace (in case not mass and bcs)
+    constants = [vec(interpolate(c, V).vector())
+                 for c in (Constant((1, 0)), Constant((0, 1)))]
 
-# Show linear solver details
-ksp.view()
+    pc.setHYPRESetEdgeConstantVectors(*constants)
+    # NOTE: term mass term is accounted for automatically by Hypre
+    # unless pc.setPoissonBetaMatrix(None)
+
+    # Set operator for the linear solver
+    ksp.setOperators(mat(A))
+
+    uh = Function(V)
+
+    ksp.solve(vec(b), vec(uh.vector()))
+    
+    niters = ksp.getIterationNumber()
+    error = errornorm(sigma_expr, uh, 'Hdiv', degree_rise=1)
+    hmin = mesh.mpi_comm().tompi4py().allreduce(mesh.hmin(), pyMPI.MIN)
+
+    return hmin, V.dim(), niters, error
+
+# --------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    msg = 'hmin = %g #dofs = %d, niters = %d, error = %g(%.2f)'
+
+    h0, error0 = None, None
+    for n in (8, 16, 32, 64, 128, 256): 
+        h, ndofs, niters, error = main(n)
+
+        if error0 is not None:
+            rate = ln(error/error0)/ln(h/h0)
+        else:
+            rate = -1
+        h0, error0 = h, error
+
+        print(msg % (h, ndofs, niters, error, rate))
