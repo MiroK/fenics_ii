c01d1f5ef6e4e05c2f0233f8d54e0f9a58af2ce3
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2018-10-26 19:48:23 +0200

    test averaging

---
+++ test/avg_test.py
@@ -0,0 +1,67 @@
+from dolfin import *
+from xii.meshing.make_mesh_cpp import make_mesh
+from xii.assembler.average_matrix import cylinder_average_matrix
+from xii import EmbeddedMesh
+import numpy as np
+
+
+def make_z_mesh(num_vertices, zmin=0, zmax=1):
+    '''{(0, 0, zmin + t*(zmax - zmin))}'''
+
+    t = zmin + np.linspace(0, 1, num_vertices)*(zmax - zmin)
+    coordinates = np.c_[np.zeros_like(t), np.zeros_like(t), t]
+    cells = np.c_[np.arange(num_vertices - 1), np.arange(1, num_vertices)]
+    cells.dtype = 'uintp'
+
+    mesh = Mesh(mpi_comm_world())
+    make_mesh(coordinates, cells, 1, 3, mesh)
+
+    return mesh
+
+
+def test(f, n, radius):
+    '''Check integrals due to averaging operator'''
+    mesh = BoxMesh(Point(-1, -1, -1), Point(1, 1, 1), n, n, n)
+
+    mf = MeshFunction('size_t', mesh, 1, 0)
+    CompiledSubDomain('near(x[0], 0.0) && near(x[1], 0.0)').mark(mf, 1)
+    line_mesh = EmbeddedMesh(mf, 1)
+
+    V = FunctionSpace(mesh, 'CG', 1)
+    TV = FunctionSpace(line_mesh, 'DG', 1)
+
+    f = interpolate(f, V)
+
+    Pi = cylinder_average_matrix(V, TV, radius, 10)
+    print '\t', Pi.norm('linf'), max(len(Pi.getrow(i)[0]) for i in range(TV.dim()))
+    
+    Pi_f = Function(TV)
+    Pi.mult(f.vector(), Pi_f.vector())
+
+    return Pi_f
+
+# --------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    radius = 0.3
+    
+    f = Expression('x[2]', degree=1)
+    Pi_f0 = f
+
+    f = Expression('x[0]*x[0] + x[1]*x[1]', degree=1)
+    Pi_f0 = Constant(radius**2)
+
+    f = Expression('x[2]*(x[0]*x[0] + x[1]*x[1])', degree=1)
+    Pi_f0 = Expression('x[2]*r*r', r=radius, degree=1)
+
+    f = Expression('x[0] + x[1]', degree=1)
+    Pi_f0 = Constant(0)
+
+    f = Expression('sin(k*pi*x[2])', k=0.5, degree=1)
+    Pi_f0 = Expression('sin(k*pi*x[2])', k=0.5, degree=1)
+
+    for n in (4, 8, 16, 32):
+        Pi_f = test(f, n, radius)
+        assert Pi_f.vector().norm('l2') > 0
+        print sqrt(abs(assemble(inner(Pi_f0 - Pi_f, Pi_f0 - Pi_f)*dx)))
