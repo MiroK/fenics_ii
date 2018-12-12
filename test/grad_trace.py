6a6a321bac703f8cde2cadd077cb03d1ae09cc41
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2018-12-12 09:58:05 +0100

    Cvrg test for normal gradient trace

--- test/grad_trace.py
+++ test/grad_trace.py
@@ -1,40 +1,58 @@
 from dolfin import *
 from xii import *
 from xii.assembler.trace_matrix import trace_mat_no_restrict
+import numpy as np
 import block
 
-mesh = UnitSquareMesh(10, 10)
-bmesh = BoundaryMesh(mesh, 'exterior')
 
-V = FunctionSpace(mesh, 'CG', 1)
-Q = FunctionSpace(bmesh, 'CG', 1)
+def check(ncells, Qelm):
+    mesh = UnitSquareMesh(*(ncells, )*2)
+    bmesh = BoundaryMesh(mesh, 'exterior')
 
-u = TrialFunction(V)
-q = TestFunction(Q)
+    V = FunctionSpace(mesh, 'CG', 1)
+    Q = FunctionSpace(bmesh, Qelm(bmesh.ufl_cell()))
 
-n = FacetNormal(mesh)
-# Want grad(u).n * q
-v = TestFunction(V)
+    u = TrialFunction(V)
+    q = TestFunction(Q)
 
-a = inner(dot(grad(u), n), v)*ds
-A = assemble(a)
-# Now take it from testV to test Q
+    n = FacetNormal(mesh)
+    # Want grad(u).n * q
+    v = TestFunction(V)
 
-T = PETScMatrix(trace_mat_no_restrict(V, Q, bmesh))
+    a = inner(dot(grad(u), n), v)*ds
+    A = assemble(a)
+    # Now take it from testV to test Q
 
-B = ii_convert(T*A)
+    T = PETScMatrix(trace_mat_no_restrict(V, Q, bmesh))
+    
+    B = ii_convert(T*A)
 
-# Now we should be able to integrate linear function exactly
-x, y = SpatialCoordinate(mesh)
+    # Now we should be able to integrate linear function exactly
+    x, y = SpatialCoordinate(mesh)
+    
+    f = 2*x + y
+    g = x - 3*y
 
-f = 2*x + y
-g = x - 3*y
+    truth = assemble(inner(dot(grad(f), n), g)*ds)
 
-truth = assemble(inner(dot(grad(f), n), g)*ds)
+    fV = interpolate(Expression('x[1] + 2*x[0]', degree=1), V)
+    gQ = interpolate(Expression('x[0] - 3*x[1]', degree=1), Q)
 
-fV = interpolate(Expression('x[1] + 2*x[0]', degree=1), V)
-gQ = interpolate(Expression('x[0] - 3*x[1]', degree=1), Q)
+    me = gQ.vector().inner(B*fV.vector())
 
-me = gQ.vector().inner(B*fV.vector())
+    return abs(truth - me)
 
-print abs(truth - me)
+# Exeact as we linears are used for testing
+assert check(ncells=8, Qelm=lambda c: FiniteElement('Lagrange', c, 1)) < 1E-14
+assert check(ncells=16, Qelm=lambda c: FiniteElement('Lagrange', c, 1)) < 1E-14
+assert check(ncells=32, Qelm=lambda c: FiniteElement('Lagrange', c, 1)) < 1E-14
+
+# Does it converge with P1-P0
+errors = []
+for n in (8, 16, 32, 64, 128):
+    errors.append(check(ncells=n,
+                        Qelm=lambda c: FiniteElement('Discontinuous Lagrange', c, 0)))
+
+errors = np.array(errors)
+
+assert np.all(np.log(errors[1:]/errors[:-1])/np.log(1./2) > 1.5)
