0b4f884b542a0e7ebd6c9f6f6944b7a0d74d3b69
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2019-05-09 11:42:53 +0200

    nc trace without intermediate CG space

--- test/coarse_trace.py
+++ test/coarse_trace.py
@@ -1,6 +1,6 @@
 from xii import *
 from dolfin import *
-
+import numpy as np
 
 n = 16
 mesh = UnitSquareMesh(*(n, )*2)
@@ -31,7 +31,10 @@ print abs(true - num)
 # ---------
 
 from xii.assembler.nonconforming_trace_matrix import nonconforming_trace_mat
+import matplotlib.pyplot as plt
 from block import block_assemble
+
+plt.figure()
 
 for n in (8, 16, 32):
     mesh = UnitSquareMesh(*(n, )*2)
@@ -41,7 +44,7 @@ for n in (8, 16, 32):
     CompiledSubDomain('near(x[0], 0)').mark(cell_f, 1)
     bmesh = EmbeddedMesh(cell_f, 1)
 
-    V = FunctionSpace(mesh, 'BDM', 1)
+    V = VectorFunctionSpace(mesh, 'DG', 1)
     bar = Expression(('x[0]', 'x[1]'), degree=1)
 
     v = interpolate(bar, V)
@@ -52,9 +55,21 @@ for n in (8, 16, 32):
     v = interpolate(bar, V)
     q = Function(Q, T*v.vector())
 
-    L = inner(bar - q, bar - q)*dx
+    L = inner(bar[0] - q[0], bar[0] - q[0])*dx
     print sqrt(abs(assemble(L)))
 
+    L = inner(bar[1] - q[1], bar[1] - q[1])*dx
+    print sqrt(abs(assemble(L)))
+
+    q0, q1 = q.split(deepcopy=True)
+
+    x = bmesh.coordinates()
+    y = np.array([q1(xi) for xi in x])
+    x = x[:, 1]
+    idx = np.argsort(x)
+    plt.plot(x[idx], y[idx], label=str(n), marker='x')
+# plt.show()
+    
 for n in (8, 16, 32):
     mesh = UnitSquareMesh(*(n, )*2)
 
@@ -63,8 +78,8 @@ for n in (8, 16, 32):
     CompiledSubDomain('near(x[0], 0)').mark(cell_f, 1)
     bmesh = EmbeddedMesh(cell_f, 1)
 
-    V = FunctionSpace(mesh, 'BDM', 1)
-    # V = VectorFunctionSpace(mesh, 'CG', 1)
+    # V = FunctionSpace(mesh, 'BDM', 1)
+    V = VectorFunctionSpace(mesh, 'DG', 1)
     Q = FunctionSpace(bmesh, 'CG', 1)
 
     v = TestFunction(V)
@@ -77,7 +92,39 @@ for n in (8, 16, 32):
     a = inner(dot(Tv, n), p)*dx_
     A = ii_assemble(a)
 
-    bar = Expression(('1+x[0]-2*x[1]', '0'), degree=1)
+    bar = Expression(('1+x[0]-2*x[1]', 'x[0]'), degree=1)
+    foo = Expression('1-3*x[0]-2*x[1]', degree=1)
+
+    bar_ = Expression('1+x[0]-2*x[1]', degree=1)   
+    true = assemble(foo*bar_*ds(domain=mesh_fine, subdomain_data=cell_f)(1))
+
+    num = interpolate(bar, V).vector().inner(A*interpolate(foo, Q).vector())
+    
+    print abs(true - num)
+
+
+for n in (8, 16, 32):
+    mesh = UnitSquareMesh(*(n, )*2)
+
+    mesh_fine = UnitSquareMesh(*(3*n, )*2)
+    cell_f = MeshFunction('size_t', mesh_fine, 1, 0)
+    CompiledSubDomain('near(x[0], 0)').mark(cell_f, 1)
+    bmesh = EmbeddedMesh(cell_f, 1)
+
+    V = FunctionSpace(mesh, 'BDM', 1)
+    Q = FunctionSpace(bmesh, 'CG', 1)
+
+    v = TestFunction(V)
+    p = TrialFunction(Q)
+    Tv = Trace(v, bmesh)
+
+    n = Constant((1, 0))
+    # The line integral
+    dx_ = Measure('dx', domain=bmesh)
+    a = inner(dot(Tv, n), p)*dx_
+    A = ii_assemble(a)
+
+    bar = Expression(('1+x[0]-2*x[1]', 'x[0]'), degree=1)
     foo = Expression('1-3*x[0]-2*x[1]', degree=1)
 
     bar_ = Expression('1+x[0]-2*x[1]', degree=1)   
--- xii/assembler/nonconforming_trace_matrix.py
+++ xii/assembler/nonconforming_trace_matrix.py
@@ -16,53 +16,6 @@ def nonconforming_trace_mat(V, T):
     Matrix taking function f from d dim space V to g in (d-1) space T. 
     T(f) ~ g should hold.
     '''
-    # We are done
-    if V.ufl_element().family() == 'Lagrange':
-        return cg_nc_trace_mat(V, T)
-
-    # FIXME: this is a workaround as the logic of cg_nc_trace_mat seems
-    # to cover only contunous Lagrange elms. The idea here is that we need
-    # to go first from V to corresponding CG space (by interpolation)
-    X = df.FunctionSpace(V.mesh(), cg_element(V.ufl_element()))
-    I = interpolation_mat(V, X)
-    
-    T = nonconforming_trace_mat(X, T)
-    # Cast the product back PETSc.Mat (to have a consistent return type)
-    return as_petsc(convert(df.PETScMatrix(T)*df.PETScMatrix(I)))
-
-
-def cg_element(elm):
-    '''Continuous Lagrange element for interpolation elm.'''
-    # Want exact match here; otherwise VectorElement is MixedElement and while
-    # it works I don't find it pretty
-    if type(elm) == df.MixedElement:
-        return df.MixedElement(map(trace_element, elm.sub_elements()))
-    # What I want
-    family = 'Lagrange'
-    degree = elm.degree()  # Preserve degree
-    cell = elm.cell()
-
-    # How to construct:
-    # There is an issue here where e.g. Hdiv are not scalars, their
-    # element is FiniteElement but we want trace space from VectorElement
-    elmtype_map = {0: df.FiniteElement,
-                   1: df.VectorElement,
-                   2: df.TensorElement}
-    # So let's check first for elements where scalar = FiniteElm, vector == VectorElm 
-    rank = len(elm.value_shape())
-    if elmtype_map[rank] == type(elm):
-        elm = type(elm)  # i.e. vector element stays vector element
-    else:
-        elm = elmtype_map[rank]
-
-    return elm(family, cell, degree)
-
-
-def cg_nc_trace_mat(V, T):
-    '''
-    Let V be a continuous Lagrange space. Matrix taking function f from 
-    d dim space V to g in (d-1) space T. T(f) ~ g should hold. 
-    '''    
     # For this to work I only make sure that function values are the same
     assert V.dolfin_element().value_rank() == T.dolfin_element().value_rank()
     assert V.ufl_element().value_shape() == T.ufl_element().value_shape()
@@ -76,7 +29,8 @@ def cg_nc_trace_mat(V, T):
     #     \/----\
     #
     # and thus is assume that each dof of T space (row) will involve
-    # collisions with several cells/basis function of V space
+    # collisions with several cells/basis function of V space. However
+    # here we only snap to the first one
     
     mesh = V.mesh()  # The (d-1)trace mesh
     tree = mesh.bounding_box_tree()
@@ -87,11 +41,11 @@ def cg_nc_trace_mat(V, T):
     # Colliding cells with trace dofs
     collisions = []
     for Tdof_x in T.tabulate_dof_coordinates().reshape((T.dim(), -1)):
-        cs = tree.compute_entity_collisions(df.Point(*Tdof_x))
-        if any(c >= limit for c in cs):
-            df.warning('Some colliding cells not found')
-            cs = filter(lambda c: c < limit, cs)
-        collisions.append(cs)
+        c = tree.compute_first_entity_collision(df.Point(*Tdof_x))
+        # Contained?
+        c >= limit and df.warning('Some colliding cells not found')
+            
+        collisions.append(c)
 
     # So we fill rows by checking basis functions of on the isected cells
     Vdm = V.dofmap()
@@ -100,7 +54,11 @@ def cg_nc_trace_mat(V, T):
     V_basis_function = FEBasisFunction(V)
     T_degree_of_freedom = DegreeOfFreedom(T)
 
-    visited_dofs = [False]*T.dim()
+    X_T = T.tabulate_dof_coordinates().reshape((T.dim(), -1))
+    X_V = V.tabulate_dof_coordinates().reshape((V.dim(), -1))
+    
+    visited_dofs = np.zeros(T.dim(), dtype=bool)
+    col_values = np.zeros(V_basis_function.elm.space_dimension(), dtype='double')
     with petsc_serial_matrix(T, V) as mat:
 
         for Tcell in range(T.mesh().num_cells()):
@@ -111,36 +69,30 @@ def cg_nc_trace_mat(V, T):
             for local_T, Tdof in enumerate(Tdofs):
                 # Seen the row?
                 if visited_dofs[Tdof]: continue
-                
+
                 visited_dofs[Tdof] = True
 
                 # Set to current dof
                 T_degree_of_freedom.dof = local_T
 
-                col_indices, col_values = [], []
                 # Now all the V cells and their basis functions
-                for c in collisions[Tdof]:
-                    # Set the dof cell
-                    V_basis_function.cell = c
+                c = collisions[Tdof]
+                # Set the dof cell
+                V_basis_function.cell = c
                     
-                    Vdofs = Vdm.cell_dofs(c)
-                    # Columns
-                    for local_V, Vdof in enumerate(Vdofs):
-                        if Vdof in col_indices:
-                            continue
-                        
-                        # Set as basis_function
-                        V_basis_function.dof = local_V
+                Vdofs = np.array(Vdm.cell_dofs(c), dtype='int32')  # These are columns
+                    
+                # Fill column
+                for local_V, Vdof in enumerate(Vdofs):
+                    # Set as basis_function
+                    V_basis_function.dof = local_V
 
-                        # Evaluate trace dof at basis function
-                        dof_value = T_degree_of_freedom.eval(V_basis_function)
-                        col_indices.append(Vdof)
-                        col_values.append(dof_value)
-                # Can fill the matrix row
-                col_indices = np.array(col_indices, dtype='int32')
-                col_values = np.array(col_values)
+                    # Evaluate trace dof at basis function
+                    dof_value = T_degree_of_freedom.eval(V_basis_function)
 
-                mat.setValues([Tdof], col_indices, col_values, PETSc.InsertMode.INSERT_VALUES)
+                    col_values[local_V] = dof_value
+
+                mat.setValues([Tdof], Vdofs, col_values, PETSc.InsertMode.INSERT_VALUES)
     return mat
 
 # --------------------------------------------------------------------
