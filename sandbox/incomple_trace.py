75072e592ed5d5759b3dfc4a2fdf0ae9c7db3aea
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2019-06-24 21:04:15 +0200

    example showing incomplete bug

--- sandbox/incomple_trace.py
+++ sandbox/incomple_trace.py
@@ -31,23 +31,70 @@ CompiledSubDomain('near(x[1], 1)').mark(cell_f, 4)
 
 iface_mesh = EmbeddedMesh(cell_f, [1, 2, 3, 4])
 
-V = FunctionSpace(iface_mesh, 'CG', 1)
-u, v = TrialFunction(V), TestFunction(V)
+Q = FunctionSpace(iface_mesh, 'CG', 1)
+p, q = TrialFunction(Q), TestFunction(Q)
 
 dx = Measure('dx', domain=iface_mesh, subdomain_data=iface_mesh.marking_function)
 
 f_ = Expression('2*x[0]-x[1]', degree=1)
-f = interpolate(f_,
-                FunctionSpace(RectangleMesh(Point(0, 0), Point(3, 3), 3*n, 3*n),
-                              'CG',
-                              1))
+f = interpolate(f_, FunctionSpace(mesh, 'CG', 1))
 
-a = inner(u, v)*dx()
-L = sum(inner(Trace(f, iface_mesh), v)*dx(i) for i in range(1, 5))
+a = inner(p, q)*dx()
+L = sum(inner(Trace(f, iface_mesh), q)*dx(i) for i in range(1, 5))
 
 A, b = map(ii_assemble, (a, L))
 
-uh = Function(V)
+uh = Function(Q)
 solve(A, uh.vector(), b)
 
 print sqrt(abs(assemble(inner(f_ - uh, f_ - uh)*dx()))), sqrt(abs(assemble(inner(uh, uh)*dx())))
+
+# --------------------------------------------------------------------
+# What if the interface is not shared
+# --------------------------------------------------------------------
+#  |---|---|
+#  |0  |2__|
+#  |  /1   |
+#  | /-----|
+
+domain0 = '(x[0] < 0.5 + tol) && (x[1] > x[0] - tol)'
+domain1 = '(x[1] < 0.5 + tol) && (x[1] < x[0] + tol)'
+domain2 = '(x[0] > 0.5-tol) && (x[1] > 0.5-tol)'
+
+iface = ['near(x[0], 0.5) && x[1] > 0.5 - tol',
+         'near(x[0], x[1]) && x[0] < 0.5 + tol',
+         'near(x[1], 0.5) && x[0] > 0.5 - tol']
+
+mesh = UnitSquareMesh(n, n)
+# Iface
+facet_f = MeshFunction('size_t', mesh, 1, 0)
+for idx, subd in enumerate(iface, 1):
+    CompiledSubDomain(subd, tol=1E-10).mark(facet_f, idx)
+
+iface = EmbeddedMesh(facet_f, (1, 2, 3))
+dx_ = Measure('dx', domain=iface, subdomain_data=iface.marking_function)
+
+# Subdomains
+cell_f = MeshFunction('size_t', mesh, 2, 0)
+for idx, subd in enumerate((domain0, domain1, domain2)):
+    CompiledSubDomain(subd, tol=1E-10).mark(cell_f, idx)
+
+f = Expression('2*x[1]-3*x[0]', degree=1)
+# Now imagine that each subdomain has a piece of interfomation and we
+# want to glue it together
+meshes = [SubMesh(mesh, cell_f, tag) for tag in range(3)]
+Vs = [FunctionSpace(mesh, 'CG', 1) for mesh in meshes]
+vs = [Trace(interpolate(f, Vi), iface) for Vi in Vs]
+
+Q = FunctionSpace(iface, 'CG', 1)
+p, q = TrialFunction(Q), TestFunction(Q)
+
+a = inner(p, q)*dx_()
+L = inner(vs[0], q)*dx_(1) + inner(vs[1], q)*dx_(2) + inner(vs[2], q)*dx_(3)
+
+A, b = map(ii_assemble, (a, L))
+
+uh = Function(Q)
+solve(A, uh.vector(), b)
+
+print sqrt(abs(assemble(inner(f - uh, f - uh)*dx_()))), sqrt(abs(assemble(inner(uh, uh)*dx_())))
